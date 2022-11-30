7cfb1fdd74160affa0f59de06c3ba067296792fa
Dumitrita Macarenco <macarencodumitrita20@stud.ase.ro>
2022-11-30 23:46:05 +0200

    Class create

--- MyProject/MyProject/Source.cpp
+++ MyProject/MyProject/Source.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
+class Location {
+	char* address = nullptr;
+	int noOfRows = 0;
+	string zone = "";
+	int noOfSeat = 0;
+
+};
+
+class Event {
+	char* eventName = nullptr;
+	float price = 0;
+	float time = 0;
+	float durationTime = 0;
+
+};
 int main() {
 	std::cout << std::endl << "Hello Dumitrita";
+	cout << endl << "Trying to update";
 }
