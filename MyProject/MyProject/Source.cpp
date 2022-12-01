11447976eecc9f4022c428ef276df7eb94596e8e
Dumitrita Macarenco <macarencodumitrita20@stud.ase.ro>
2022-12-01 13:18:57 +0200

    Class update

--- MyProject/MyProject/Source.cpp
+++ MyProject/MyProject/Source.cpp
@@ -7,7 +7,12 @@ class Location {
 	int noOfRows = 0;
 	string zone = "";
 	int noOfSeat = 0;
+public:
+	static int MIN_NO_OF_SEATS;
 };
+int Location::MIN_NO_OF_SEATS = 50;
+
+enum EventType { Football = 1, Movie = 2, Theater = 3, Concert = 4, ComedyClub = 5, Another = 6 };
 
 class Event {
 	int eventId = 0;
@@ -17,7 +22,13 @@ class Event {
 	float durationTime = 0;
 	int* soldItemsEachEvent = nullptr;
 	int noEvents;
+	EventType type;
+public:
+	static int MIN_EVENT_LENGHT;
+	static int MAX_NO_TICKETS;
 };
+int Event::MIN_EVENT_LENGHT = 3;
+int Event::MAX_NO_TICKETS = 10000;
 
 class Person { // is the person that is buying a ticket for an event
 	string name = "";
@@ -25,15 +36,25 @@ class Person { // is the person that is buying a ticket for an event
 	bool has18 = false;
 	int* noTicketsBought = nullptr;
 	int noTickets = 0;
+public:
+	static int MIN_NAME_LENGHT;
 };
+int Person::MIN_NAME_LENGHT = 3;
+
+enum TicketType{Regular = 1, VIP = 2, Family =3}; // by family i'm trying to say that the person will have a special seat. Ex: near the stage, or on the stage :)
 
 class Ticket {
 	const int ticketID = 0;
 	float price = 0;
 	char* ticketDescription;
 	//const int codeBarNumber; not sure yet.
-
+	TicketType type;
+public:
+	static float MIN_PRICE;
+	static int NO_TICKETS_BOUGHT;//bc a person can't buy 0 tickets
 };
+float Ticket::MIN_PRICE = 50;
+int Ticket::NO_TICKETS_BOUGHT = 1;
 
 
 int main() {
