b8c4d9feed31254135176bd895a424b687013dab
Dumitrita Macarenco <macarencodumitrita20@stud.ase.ro>
2022-12-01 14:31:31 +0200

    Adding get and set methods to Location class

--- MyProject/MyProject/Source.cpp
+++ MyProject/MyProject/Source.cpp
@@ -6,11 +6,83 @@ class Location {
 	char* address = nullptr;
 	int noOfRows = 0;
 	string zone = "";
-	int noOfSeat = 0;
+	int noOfSeats = 0;
+	bool isAvailable = false;
 public:
 	static int MIN_NO_OF_SEATS;
+	static int ADDRESS_LENGHT;
+
+	bool IsAvailable() {
+		return this->isAvailable;
+	}
+
+	string getZone() {
+		return this->zone;
+	}
+
+	int getNoOfRows() {
+		return this->noOfRows;
+	}
+
+	int getNoOfSeats() {
+		return this->noOfSeats;
+	}
+
+	void setAddress(const char* newAddress) {
+		if (newAddress == nullptr && strlen(newAddress) < Location::ADDRESS_LENGHT) {
+			throw exception("Address is either to short or null");
+		}
+
+		if (this->address != nullptr) {
+			delete[] this->address;
+		}
+
+		this->address = new char[strlen(newAddress) + 1];
+		strcpy_s(this->address, strlen(newAddress) + 1, newAddress);
+	}
+
+	void setZone(string zone) {
+		if (zone.length() >= 1) { // the name for the zone can be "1" or "2" that's why i limited the lenght to 1.
+			this->zone = zone;
+		}
+		else {
+			throw exception("The name for the zone is too short");
+		}
+	}
+
+	void setNoOfRows(int newNoOfRows) {
+		if (newNoOfRows <= 0) {
+			throw exception("It can't have  a negative number of rows");
+		}
+		this->noOfRows = newNoOfRows;
+		if (this->noOfRows == 0) {
+			this->isAvailable = false;
+		}
+		else {
+			this->isAvailable = true;
+		}
+	}
+
+	void setNoOfSeats(int newNoOfSeats) {
+		if (newNoOfSeats <= 0) {
+			throw exception("The number of seats can't be a negative number");
+		}
+		this->noOfSeats = newNoOfSeats;
+		if (this->noOfSeats == 0) {
+			this->isAvailable = false;
+		}
+		else {
+			this->isAvailable = true;
+		}
+	}
+
+
+
+
+
 };
 int Location::MIN_NO_OF_SEATS = 50;
+int Location::ADDRESS_LENGHT = 3;
 
 enum EventType { Football = 1, Movie = 2, Theater = 3, Concert = 4, ComedyClub = 5, Another = 6 };
 
@@ -29,6 +101,7 @@ public:
 };
 int Event::MIN_EVENT_LENGHT = 3;
 int Event::MAX_NO_TICKETS = 10000;
+
 
 class Person { // is the person that is buying a ticket for an event
 	string name = "";
